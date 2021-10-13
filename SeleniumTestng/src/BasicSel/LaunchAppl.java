21cbfc25db56e72df031dce5df12f3ce11753b41
nancygourav <nancygourav581@gmail.com>
2021-10-13 10:38:24 +0530

    First Commit

---
+++ SeleniumTestng/src/BasicSel/LaunchAppl.java
@@ -0,0 +1,19 @@
+package BasicSel;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class LaunchAppl {
+  @Test
+  public void f() {
+  }
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
